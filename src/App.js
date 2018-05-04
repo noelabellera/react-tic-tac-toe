ee4962ade31fbffb28cecfd38b13436200fc73d3
Noel Abellera <noel.abellera@gmail.com>
2018-05-03 20:57:01 -0700

    change title

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
   render() {
     return (
       <div className="game">
-        <h1>Tic-Tac-Toe</h1>
+        <h1>Welcome to React-Tic-Tac-Toe</h1>
         <div className="game-board">
           <Board calculateWinner={this.calculateWinner} 
                  handleReset={this.handleReset} />
